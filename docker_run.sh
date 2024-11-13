c9473bdb53b8f68efe027f910a4d2865ab9986f5
JakobDomislovic <jakob.domislovic2@gmail.com>
2024-11-13 08:09:43 +0100

    fix distro/

--- docker_run.sh
+++ docker_run.sh
@@ -73,5 +73,5 @@ docker run \
   --env DISPLAY=$DISPLAY \
   --env TERM=xterm-256color \
   --name $full_container_name \
-  preddiplproj2024:focal-no-gpu \
+  preddiplproj2024:$distro \
   /bin/bash
